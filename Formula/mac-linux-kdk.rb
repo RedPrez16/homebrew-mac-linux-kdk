7060bf43811b4e019cd85d9f5e2a043a9759e86b
Kenneth Endfinger <kaendfinger@gmail.com>
2021-11-05 23:11:33 -0400

    Update formula with latest fix.

--- Formula/mac-linux-kdk.rb
+++ Formula/mac-linux-kdk.rb
@@ -1,10 +1,10 @@
 class MacLinuxKdk < Formula
   desc "Linux Kernel Development Kit for macOS"
   homepage "https://github.com/SpinlockLabs/mac-linux-kdk"
-  version "2021.11.05-20bb15c1bd"
+  version "2021.11.05-8192041c2c"
 
-  url "https://github.com/SpinlockLabs/mac-linux-kdk/archive/20bb15c1bdeee37f25cde4570843079b9490322c.zip"
-  sha256 "a1ac64bc4feb388714f6da20dfdec72d7026ce814380bb73733ec0ba09fbc0af"
+  url "https://github.com/SpinlockLabs/mac-linux-kdk/archive/8192041c2cd1a5f31ff1b0663a6b8be4f42c207a.zip"
+  sha256 "db745202ef72842dc8b9b6b792c82b2398c44fcce41a972610b0c963976061bc"
   license "LGPL-3.0-or-later" => { with: "LGPL-3.0-linking-exception" }
 
   depends_on "llvm"
