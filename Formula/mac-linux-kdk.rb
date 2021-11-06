62a662af30cfc544e2c397e6b80167d3b754d287
Kenneth Endfinger <kaendfinger@gmail.com>
2021-11-05 22:54:16 -0400

    ARM64 Build Support

--- Formula/mac-linux-kdk.rb
+++ Formula/mac-linux-kdk.rb
@@ -1,14 +1,19 @@
 class MacLinuxKdk < Formula
-    desc "Linux Kernel Development Kit for macOS"
-    homepage "https://github.com/SpinlockLabs/mac-linux-kdk"
-    version "2021.11.05-efa34e3d4e"
+  desc "Linux Kernel Development Kit for macOS"
+  homepage "https://github.com/SpinlockLabs/mac-linux-kdk"
+  version "2021.11.05-efa34e3d4e"
 
-    url "https://github.com/SpinlockLabs/mac-linux-kdk/archive/efa34e3d4ed1be3fb6aadb5af2297a98f94041f4.zip"
-    sha256 "cb1fa50efdecc8241567f88178f17b45c7c49f5ecd0e9b3083cd23806a0d5779"
-    license "LGPL-3.0-or-later" => { with: "LGPL-3.0-linking-exception" }
-  
-    def install
-      libexec.install Dir["env/*"]
-    end
+  url "https://github.com/SpinlockLabs/mac-linux-kdk/archive/20bb15c1bdeee37f25cde4570843079b9490322c.zip"
+  sha256 "a1ac64bc4feb388714f6da20dfdec72d7026ce814380bb73733ec0ba09fbc0af"
+  license "LGPL-3.0-or-later" => { with: "LGPL-3.0-linking-exception" }
+
+  depends_on "llvm"
+  depends_on "make"
+  depends_on "ncurses"
+  depends_on "openssl@1.1"
+
+  def install
+    libexec.install Dir["env/*"]
+    bin.install Dir["bin/*"]
   end
-  
+end
