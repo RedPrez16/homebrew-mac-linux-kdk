97d2bd8f5414e6d753f0fb1f7843ac70b5529a0f
Kenneth Endfinger <kaendfinger@gmail.com>
2021-11-05 23:04:58 -0400

    Update version field.

--- Formula/mac-linux-kdk.rb
+++ Formula/mac-linux-kdk.rb
@@ -1,7 +1,7 @@
 class MacLinuxKdk < Formula
   desc "Linux Kernel Development Kit for macOS"
   homepage "https://github.com/SpinlockLabs/mac-linux-kdk"
-  version "2021.11.05-efa34e3d4e"
+  version "2021.11.05-20bb15c1bd"
 
   url "https://github.com/SpinlockLabs/mac-linux-kdk/archive/20bb15c1bdeee37f25cde4570843079b9490322c.zip"
   sha256 "a1ac64bc4feb388714f6da20dfdec72d7026ce814380bb73733ec0ba09fbc0af"
