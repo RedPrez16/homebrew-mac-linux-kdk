ca0a42465879dfba3893eef4b907a64352f76944
Kenneth Endfinger <kaendfinger@gmail.com>
2021-11-07 21:38:49 -0500

    mac-linux-kdk: update to 22c8f765d3f54c688071e0e86ab875ed9ac98806

--- Formula/mac-linux-kdk.rb
+++ Formula/mac-linux-kdk.rb
@@ -1,12 +1,20 @@
 class MacLinuxKdk < Formula
   desc "Linux Kernel Development Kit for macOS"
   homepage "https://github.com/SpinlockLabs/mac-linux-kdk"
-  version "2021.11.05-7cf9dfd"
+  version "2021.11.07-22c8f765d3"
 
-  url "https://github.com/SpinlockLabs/mac-linux-kdk/archive/7cf9dfd65637e0af1cc52730d025f76523996407.zip"
-  sha256 "247291c1acb0a5d8dbe2d7321838f0c093c57f965f8fa4a6d3ae3836b7e24c88"
+  url "https://github.com/SpinlockLabs/mac-linux-kdk/archive/22c8f765d3f54c688071e0e86ab875ed9ac98806.zip"
+  sha256 "708ebefe491c8c751c50be7cce323ab1fa5c53ca9aba9a735b69a00ce47d802d"
   license "LGPL-3.0-or-later" => { with: "LGPL-3.0-linking-exception" }
 
+  depends_on "coreutils"
+  depends_on "file-formula"
+  depends_on "findutils"
+  depends_on "gawk"
+  depends_on "gnu-sed"
+  depends_on "gnu-tar"
+  depends_on "grep"
+  depends_on "libtool"
   depends_on "llvm"
   depends_on "make"
   depends_on "ncurses"
